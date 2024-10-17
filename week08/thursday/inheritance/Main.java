3e42c5a78f14d793876b478c8e7b6e6ab12f2f97
Jin An <jin.an@dtcc.edu>
2024-10-17 09:54:16 -0400

    Create Main.java

---
+++ week08/thursday/inheritance/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+//        Bowler b1 = new Bowler();
+//        b1.setBallWeight(15.8);
+//
+//        b1.setName("Haider");
+//        b1.setRank(350);
+//
+//        b1.setJobDescription("Throw heavy object down the lane to knock down pins!");
+//        b1.setContractAmount(29000);
+//
+//        System.out.println("B1 likes to bowl with a " + b1.getBallWeight() + " ball");
+//
+        SoccerPlayer messi = new SoccerPlayer("Messi");
+        messi.setGoalsScored(3);
+        messi.setRank(1);
+
+        messi.setContractAmount(400000000);
+
+//        FootballPlayer kelce = new FootballPlayer();
+//        kelce.setFortyTime(4.2);
+//        kelce.setName("Taylor's boyfriend");
+
+//        Contractor c1 = new Contractor();
+//        c1.setJobDescription("Catch passes.");
+//        c1.setContractAmount(150000000);
+
+
+
+
+
+    }
+}
